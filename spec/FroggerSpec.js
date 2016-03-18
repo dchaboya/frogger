8842299a3606527717b55a6ae50ef3530543c7f8
Daniel Chaboya <Danielc@reliablecontrols.com>
2016-03-17 17:08:01 -0700

    Test enemy constructor for sprite property

--- spec/FroggerSpec.js
+++ spec/FroggerSpec.js
@@ -1,3 +1,18 @@
 describe("Frogger", function() {
 
 });
+
+describe("Enemy Constructor", function() {
+	var foo;
+
+	beforeEach(function() {
+		foo = {
+			sprite: "images/enemy-bug.png"
+		}
+	});
+
+	it("Should return object containing sprite property", function() {
+		var enemy1 = new Enemy();
+		expect(enemy1.sprite).toEqual(foo.sprite);
+	});
+});
